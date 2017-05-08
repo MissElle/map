a37e704de10c0ed2c431cec95027ead774568939
MissElle <elli.g.pope@gmail.com>
2017-05-08 16:55:55 -0700

    messed around making a function to try and alter current map but was unsuccessful. Decided to put the geolocation options in a variable as seen with Mozilla Developer. Will try another function to change latitude and longitude.

--- script.js
+++ script.js
@@ -2,6 +2,46 @@
 
 //------------------------------------------------------------------------------------//
 
+var navGeo = navigator.geolocation;
+
+//if (navGeo) {
+//  nav.GeoLoc.getCurrentPosition(
+//    function successCallback(position){
+//      console.log("great!")  
+//    },
+//    function errorCallback (error) {
+//      var message;
+//      switch (error.code) {
+//        case error.PERMISSION_DENIED:
+//          message = "Geolocation permission denied.";
+//          break;
+//          
+//        case error.POSITION_UNAVAILABLE:
+//          message = "Geolocation position unavailable."
+//          break;
+//          
+//        case error.TIMEOUT:
+//          message = "Geolocation timed out."
+//          break;
+//          
+//        default:
+//          message += '\n' + error.message;
+//          document.getElementById('map').innerHTML(message);
+//    }
+//      enableHighAccuracy: false,
+//      maximumAge: 60000,
+//      timeout: 30000
+//    });
+//}else {
+//  initMap();
+//}
+
+var geoOptions = {
+  enableHighAccuracy: true, 
+  maximumAge        : 30000, 
+  timeout           : 27000
+};
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 47.618107, lng:-122.319391},
