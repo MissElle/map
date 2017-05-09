abebfe34e640fb7ade53c96f5348ee43654dc791
MissElle <elli.g.pope@gmail.com>
2017-05-08 17:10:34 -0700

    attempts at making an if/else statement to alter map positions still failed. Will need to look elsewhere.

--- script.js
+++ script.js
@@ -36,16 +36,52 @@ var navGeo = navigator.geolocation;
 //  initMap();
 //}
 
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+   if (geoSuccess) {
+     center: {lat:position.coords.latitude, lng:position.coords.longitude},
+     zoom: 16;
+   }else {
+    center: {lat: 47.618107, lng:-122.319391},
+    zoom: 16
+    }
+    });
+  console.log("map is being read");
+}
+
+//------------------------------------------------------------------------------------//
+
+//The code below grabs the current position and watches it
+
+function geoSuccess(position) {
+  center: {position.coords.latitude, position.coords.longitude}
+}
+
+function geoError() {
+  var message;
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      message = "Geolocation permission denied.";
+      break;
+          
+    case error.POSITION_UNAVAILABLE:
+      message = "Geolocation position unavailable."
+      break;
+          
+    case error.TIMEOUT:
+      message = "Geolocation timed out."
+      break;
+          
+    default:
+      message += '\n' + error.message;
+      document.getElementById('map').innerHTML = message;
+    }
+}
+
 var geoOptions = {
   enableHighAccuracy: true, 
   maximumAge        : 30000, 
   timeout           : 27000
 };
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.618107, lng:-122.319391},
-    zoom: 16
-    });
-  console.log("map is being read");
-}
+var watchGeo = navigator.geolocation.watchPosition(geoSuccess, geoError, geoOptions);
