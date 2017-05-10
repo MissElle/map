f8bc07459b7d91e48eda4707689f4f3a1d751fe9
Elle <elli.g.pope@gmail.com>
2017-05-10 13:30:34 -0700

    using google's interface for generating a map, able to make the custom location pull when user loads the map

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.getElementById('myCoords').addEventListener("change", initMap);
 //Function for generating map below
 
 function initMap() {
+  infoWindow = new google.maps.InfoWindow;
   var selForm = document.getElementById('favPlaces');
   var selOptions = selForm.options[selForm.selectedIndex].text;
   var mapLat = parseFloat(document.getElementById('myLat').value);
@@ -22,30 +23,54 @@ function initMap() {
 
 //These if/else statements determine coordinates depending which option is selected
   
-  if(selOptions === "Enter Coordinates, or..."){
+  if(selOptions === "Enter Coordinates"){
     mapLat = parseFloat(document.getElementById('myLat').value);
-    mapLng = parseFloat(document.getElementById('myLng').value);    
+    mapLng = parseFloat(document.getElementById('myLng').value);
+    infoWindow.setContent('Here are your Custom Coordinates');
   }else if(selOptions === "Cal Anderson Park"){
     mapLat = 47.618107;
     mapLng = -122.319391;
+    infoWindow.setContent('Cal Anderson Park');
   }else if(selOptions === "Uwajimaya"){
     mapLat = 47.596772;
     mapLng = -122.326845;
+    infoWindow.setContent('Uwajimaya');
   }else if(selOptions === "NYX"){
     mapLat = 47.620885;
     mapLng = -122.321206;
+    infoWindow.setContent('NYX');
   }else if(selOptions === "UW Extension"){
     mapLat = 47.608419;
     mapLng = -122.3378477;
+    infoWindow.setContent('UW Extension');
   }else if(selOptions === "Convention Center"){
     mapLat = 47.6117273;
     mapLng = -122.3351027;
+    infoWindow.setContent('Washington State Convention Center');
   }else if(selOptions === "Kobe Terrace"){
     mapLat = 47.6002647;
     mapLng = -122.3264266;
+    infoWindow.setContent('Kobe Terrace');
   }else {
-      mapLat = position.coords.latitude;
-      mapLng = position.coords.longitude;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+  
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('You are Here');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+                                           
+      function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    }else {
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
   }
  
 //Map is generated with the code below
@@ -54,6 +79,10 @@ function initMap() {
     center: {lat: mapLat, lng: mapLng},
     zoom: mapZoom
     });
-  console.log("map is being read");
+//------------------------------------------------------------------------------------//
+//This function will get the coordinates
+
+
+    console.log("map is being read");
 }
 
