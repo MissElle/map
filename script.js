8f62980977a027808ac258a2a958d20e209148f1
Elle <elli.g.pope@gmail.com>
2017-05-10 10:01:24 -0700

    added an event listener to watch when the form changes to update the coordinates. Coordinates are super picky and need to be exact or will not change properly. Will now work on getting the locations coordinates added

--- script.js
+++ script.js
@@ -2,20 +2,22 @@
 
 //------------------------------------------------------------------------------------//
 
-var geoOptions = {
-  enableHighAccuracy: true, 
-  maximumAge        : 30000, 
-  timeout           : 27000
-};
+//var geoOptions = {
+//  enableHighAccuracy: true, 
+//  maximumAge        : 30000, 
+//  timeout           : 27000
+//}; 
 
-var mapLat = parseFloat(document.getElementById('myLat').value);
-var mapLng = parseFloat(document.getElementById('myLng').value);
-var mapZoom = parseFloat(document.getElementById('myZoom').value);
+document.getElementById('myCoords').addEventListener("change", initMap);
 
 function initMap() {
+  var mapLat = parseFloat(document.getElementById('myLat').value);
+  var mapLng = parseFloat(document.getElementById('myLng').value);
+  var mapZoom = parseFloat(document.getElementById('myZoom').value);
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: mapLat, lng: mapLng},
     zoom: mapZoom
     });
   console.log("map is being read");
 }
+
