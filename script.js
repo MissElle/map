bd72bbc864690fc0d90c316706d54db99823d78e
Elle <elli.g.pope@gmail.com>
2017-05-09 21:19:27 -0700

    changed the variables to include the parseFloat statement. This fixed the issue with the map not loading properly

--- script.js
+++ script.js
@@ -8,18 +8,14 @@ var geoOptions = {
   timeout           : 27000
 };
 
-var mapLat = document.getElementById('myLat').value;
-var mapLng = document.getElementById('myLng').value;
-var mapZoom = document.getElementById('myZoom').value;
+var mapLat = parseFloat(document.getElementById('myLat').value);
+var mapLng = parseFloat(document.getElementById('myLng').value);
+var mapZoom = parseFloat(document.getElementById('myZoom').value);
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: mapLat, lng: mapLng},
-    zoom: myZoom
+    zoom: mapZoom
     });
   console.log("map is being read");
-}
-
-function getCenter() {
-  
 }
