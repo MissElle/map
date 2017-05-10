b41cf7b82d2c61209247c0aa31a0574c2df84f88
Elle <elli.g.pope@gmail.com>
2017-05-10 14:23:59 -0700

    Put the infoWindow code into the map initialization portion of the function. This pulls up a custom marker for the map

--- script.js
+++ script.js
@@ -8,10 +8,10 @@
 //  timeout           : 27000
 //}; 
 
-document.getElementById('myCoords').addEventListener("change", initMap);
+document.getElementById('myCoords').addEventListener('change', initMap);
 
 //------------------------------------------------------------------------------------//
-//Function for generating map below
+//One function below, will generate the map, pull up current location coordinates and allow user's to pick favorite locations.
 
 function initMap() {
   infoWindow = new google.maps.InfoWindow;
@@ -20,38 +20,44 @@ function initMap() {
   var mapLat = parseFloat(document.getElementById('myLat').value);
   var mapLng = parseFloat(document.getElementById('myLng').value);
   var mapZoom = parseFloat(document.getElementById('myZoom').value);
-
+  var locName = "";
+  
 //These if/else statements determine coordinates depending which option is selected
   
   if(selOptions === "Enter Coordinates"){
     mapLat = parseFloat(document.getElementById('myLat').value);
     mapLng = parseFloat(document.getElementById('myLng').value);
     infoWindow.setContent('Here are your Custom Coordinates');
+
   }else if(selOptions === "Cal Anderson Park"){
     mapLat = 47.618107;
     mapLng = -122.319391;
-    infoWindow.setContent('Cal Anderson Park');
+    locName= "Cal Anderson<br>Park";
   }else if(selOptions === "Uwajimaya"){
     mapLat = 47.596772;
     mapLng = -122.326845;
-    infoWindow.setContent('Uwajimaya');
+    locName= "Uwajimaya";
   }else if(selOptions === "NYX"){
     mapLat = 47.620885;
     mapLng = -122.321206;
-    infoWindow.setContent('NYX');
+    locName= "NYX <br> New York Exchange";
   }else if(selOptions === "UW Extension"){
     mapLat = 47.608419;
     mapLng = -122.3378477;
-    infoWindow.setContent('UW Extension');
+    locName= "UW Extension<br>Continuing Education";
   }else if(selOptions === "Convention Center"){
     mapLat = 47.6117273;
     mapLng = -122.3351027;
-    infoWindow.setContent('Washington State Convention Center');
+    locName= "Washington State<br>Convention Center";
   }else if(selOptions === "Kobe Terrace"){
     mapLat = 47.6002647;
     mapLng = -122.3264266;
-    infoWindow.setContent('Kobe Terrace');
+    locName= "Kobe Terrace";
   }else {
+  //-----------------------------------------------------------------------------------//
+//This function will get the coordinates; altered from google's recommended coding at
+//https://developers.google.com/maps/documentation/javascript/examples/map-geolocation
+    locName= "Custom Coordinates";
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
@@ -79,10 +85,10 @@ function initMap() {
     center: {lat: mapLat, lng: mapLng},
     zoom: mapZoom
     });
-//------------------------------------------------------------------------------------//
-//This function will get the coordinates
-
-
+    infoWindow.setPosition({lat: mapLat, lng: mapLng});
+    infoWindow.setContent(locName);
+    infoWindow.open(map);
+    map.setCenter({lat: mapLat, lng: mapLng});
     console.log("map is being read");
 }
 
