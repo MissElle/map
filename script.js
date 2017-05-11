4166f1689443830b85bf6a9a28cbc3efb78a52df
MissElle <elli.g.pope@gmail.com>
2017-05-11 14:17:39 -0700

    wrote a variable to hold the div element that contains the video tag. Have to change the innerHTML of the div element, not the video tag... changing the video tag innerHTML works, but only once.

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 // script.js - script for map project
 
 //------------------------------------------------------------------------------------//
-//Event Listener to update form with any changes to the fields
-//var geoOptions = {
-//  enableHighAccuracy: true, 
-//  maximumAge        : 30000, 
-//  timeout           : 27000
-//}; 
+//Event Listener to update form with any changes to the fields 
 
 document.getElementById('myCoords').addEventListener('change', initMap);
 
@@ -21,6 +16,7 @@ function initMap() {
   var mapLng = parseFloat(document.getElementById('myLng').value);
   var mapZoom = parseFloat(document.getElementById('myZoom').value);
   var locName = "";
+  var vidSrc = document.getElementById('movie');
   
 //These if/else statements determine coordinates depending which option is selected
   
@@ -28,36 +24,43 @@ function initMap() {
     mapLat = parseFloat(document.getElementById('myLat').value);
     mapLng = parseFloat(document.getElementById('myLng').value);
     locName = "Custom Coordinates";
-
+    vidSrc.innerHTML = '<video controls><source src="videos/intro.mp4"></video>';
   }else if(selOptions === "Cal Anderson Park"){
     mapLat = 47.618107;
     mapLng = -122.319391;
     locName= "Cal Anderson<br>Park";
-  }else if(selOptions === "Uwajimaya"){
-    mapLat = 47.596772;
-    mapLng = -122.326845;
-    locName= "Uwajimaya";
-  }else if(selOptions === "NYX"){
-    mapLat = 47.620885;
+  }else if(selOptions === "Dick's Burgers"){
+    mapLat = 47.619316;
     mapLng = -122.321206;
-    locName= "NYX <br> New York Exchange";
+    locName= "Dick's Burgers<br>Broadway Street";
   }else if(selOptions === "UW Extension"){
     mapLat = 47.608419;
-    mapLng = -122.3378477;
+    mapLng = -122.320976;
     locName= "UW Extension<br>Continuing Education";
   }else if(selOptions === "Convention Center"){
     mapLat = 47.6117273;
     mapLng = -122.3351027;
     locName= "Washington State<br>Convention Center";
-  }else if(selOptions === "Kobe Terrace"){
+  }else if(selOptions === "Seattle Public Library") {
     mapLat = 47.6002647;
     mapLng = -122.3264266;
+    locName= "Seattle Public<br>Library";
+  }else if(selOptions === "Kobe Terrace"){
+    mapLat = 47.606473;
+    mapLng = -122.332990;
     locName= "Kobe Terrace";
+    vidSrc.innerHTML = '<video controls><source src="videos/5-kobe.mp4"></video>';
+  }else if(selOptions === "Uwajimaya"){
+    mapLat = 47.596772;
+    mapLng = -122.326845;
+    locName= "Uwajimaya<br>International District";
+    vidSrc.innerHTML = '<video controls><source src="videos/6-uwa.mp4"></video>';
   }else {
   //-----------------------------------------------------------------------------------//
 //This function will get the coordinates; altered from google's recommended coding at
 //https://developers.google.com/maps/documentation/javascript/examples/map-geolocation
-    locName= "Custom Coordinates";
+    locName= "You are Here";
+    vidSrc.innerHTML = '<video controls><source src="videos/intro.mp4"></video>';
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
