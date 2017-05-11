8f687864fa5eb145a6d6f679603d395867294868
MissElle <elli.g.pope@gmail.com>
2017-05-11 15:22:19 -0700

    added and compressed all videos. Edited the script to load all videos, Tested and they load fine. Final code

--- script.js
+++ script.js
@@ -25,36 +25,46 @@ function initMap() {
     mapLng = parseFloat(document.getElementById('myLng').value);
     locName = "Custom Coordinates";
     vidSrc.innerHTML = '<video controls><source src="videos/intro.mp4"></video>';
+  }else if(selOptions === "The Mercury"){
+    mapLat = 47.612839;
+    mapLng = -122.318911;
+    locName= "The Mercury<br>@MachineWorks";
+    vidSrc.innerHTML = '<video controls><source src="videos/0-merc.mp4"></video>';
   }else if(selOptions === "Cal Anderson Park"){
     mapLat = 47.618107;
-    mapLng = -122.319391;
+    mapLng = -122.318909;
     locName= "Cal Anderson<br>Park";
+    vidSrc.innerHTML = '<video controls><source src="videos/1-cal.mp4"></video>';
   }else if(selOptions === "Dick's Burgers"){
     mapLat = 47.619316;
     mapLng = -122.321206;
     locName= "Dick's Burgers<br>Broadway Street";
+    vidSrc.innerHTML = '<video controls><source src="videos/2-bro.mp4"></video>';
+  }else if(selOptions === "Convention Center"){
+    mapLat = 47.611831;
+    mapLng = -122.332323;
+    locName= "Washington State<br>Convention Center";
+    vidSrc.innerHTML = '<video controls><source src="videos/3-con.mp4"></video>';
   }else if(selOptions === "UW Extension"){
     mapLat = 47.608419;
     mapLng = -122.320976;
     locName= "UW Extension<br>Continuing Education";
-  }else if(selOptions === "Convention Center"){
-    mapLat = 47.6117273;
-    mapLng = -122.3351027;
-    locName= "Washington State<br>Convention Center";
+    vidSrc.innerHTML = '<video controls><source src="videos/4-uwex.mp4"></video>';
   }else if(selOptions === "Seattle Public Library") {
     mapLat = 47.6002647;
     mapLng = -122.3264266;
     locName= "Seattle Public<br>Library";
+    vidSrc.innerHTML = '<video controls><source src="videos/5-lib.mp4"></video>';
   }else if(selOptions === "Kobe Terrace"){
     mapLat = 47.606473;
     mapLng = -122.332990;
     locName= "Kobe Terrace";
-    vidSrc.innerHTML = '<video controls><source src="videos/5-kobe.mp4"></video>';
+    vidSrc.innerHTML = '<video controls><source src="videos/6-kobe.mp4"></video>';
   }else if(selOptions === "Uwajimaya"){
     mapLat = 47.596772;
     mapLng = -122.326845;
     locName= "Uwajimaya<br>International District";
-    vidSrc.innerHTML = '<video controls><source src="videos/6-uwa.mp4"></video>';
+    vidSrc.innerHTML = '<video controls><source src="videos/7-uwa.mp4"></video>';
   }else {
   //-----------------------------------------------------------------------------------//
 //This function will get the coordinates; altered from google's recommended coding at
@@ -92,6 +102,5 @@ function initMap() {
     infoWindow.setContent(locName);
     infoWindow.open(map);
     map.setCenter({lat: mapLat, lng: mapLng});
-    console.log("map is being read");
 }
 
